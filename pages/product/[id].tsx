1224d0abec4114a0e17019230aa53eef230639d2
kerlos1256 <kero24862@gmail.com>
2022-09-13 10:38:13 +0200

    update

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -2,7 +2,7 @@ import { NextPage } from "next";
 import React from "react";
 
 const ProductDetails: NextPage = () => {
-  return <div>product</div>;
+  return <div></div>;
 };
 
 export default ProductDetails;
